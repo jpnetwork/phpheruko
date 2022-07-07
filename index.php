feb81c2b6814797c9219cdaffe0565d7df183f5f
JP <panjantra@gmail.com>
2022-07-07 14:44:44 +0700

    add show qr

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
-echo __DIR__;
+use chillerlan\QRCode\{QRCode, QROptions}; 
+
+$code_data = base64_encode('SUJANEPANJAN'.time());
+$myqrcode = (new QRCode)->render($code_data); //QR Code
+//$generatorPNG = new \Picqer\Barcode\BarcodeGeneratorPNG();
+//$data['mybarcode'] = base64_encode($generatorPNG->getBarcode($code_data, $generatorPNG::TYPE_CODE_128,2,120));
+echo  '<img src="'.$myqrcode.'" class="img-fluid" width="350">';
