9fbbd7624cddd0af9a27b7b696d5949b7d46394f
JPNET <43113652+jpnetwork@users.noreply.github.com>
2022-07-08 12:05:47 +0700

    update

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = mysqli_connect("103.22.182.6","jpn_heroku","cGVl4lwd","jpn_heroku");
+$mysqli = mysqli_connect("localhost","dbuser","dbpass","dbname");
 	if (mysqli_connect_errno()){
 		echo "Failed to connect to Database: " . mysqli_connect_error();
 		exit();
--- info.php
+++ info.php
@@ -1,4 +1,4 @@
 <?php
 #test UPDATE
-phpinfo() ;
+#phpinfo() ;
 
